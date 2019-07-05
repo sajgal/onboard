e6f0eb634c7d6e332dfdcbc054f89389b5fee76d
Juraj <jkorbacka@gmail.com>
2019-07-05 22:51:38 +0200

    use of users browser language preferences

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import graphql from 'graphql';
 import { getUserLangKey } from 'ptz-i18n';
 import { withPrefix } from "gatsby-link";
+import browserLang from 'browser-lang';
+const languages = require('../data/languages')
 
 class RedirectIndex extends React.PureComponent {
   constructor(args) {
     super(args);
 
     if (typeof window !== 'undefined') {
-      const homeUrl = withPrefix(`/${args.data.site.siteMetadata.languages.defaultLangKey}/`);
+      let language = browserLang();
+      if (!languages.langs.includes(language))
+        language = languages.defaultLangKey;
+      const homeUrl = withPrefix(`/${language}/`);
       window.___history.replace(homeUrl);
     }
   }
@@ -20,14 +24,3 @@ class RedirectIndex extends React.PureComponent {
 
 export default RedirectIndex;
 
-export const pageQuery = graphql`
-  query IndexQuery {
-    site{
-      siteMetadata{
-        languages {
-          defaultLangKey
-        }
-      }
-    }
-  }
-`;
